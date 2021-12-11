b35f57b1bddf9d6f063b864f139f43a8a599a04f
seanhuh-dev <seanhuh.dev@gmail.com>
2021-12-11 15:59:21 +0900

    Add: router and controller

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const PORT = 5000;
+const PORT = 6000;
 
 const app = express();
 app.use(express.json()); /* bodyParser - req.body */
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
+// path와 handler를 각각 router와 controller로 분리
+const productRoutes = require("./routes");
+app.use("/api/products", productRoutes);
 
 app.listen(PORT, () => {
   console.log(`Running on port ${PORT}`);
